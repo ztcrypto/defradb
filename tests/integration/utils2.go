554e2c947844d0748de832afee2b7038a45fafee
Islam Aleiv <islam@source.network>
2023-07-18 14:04:06 +0200

    Fix after lens rebase

--- tests/integration/utils2.go
+++ tests/integration/utils2.go
@@ -1601,6 +1601,7 @@ func executeSubscriptionRequest(
 							action.ExpectedError,
 							nil,
 							// anyof is not yet supported by subscription requests
+							nil,
 							0,
 							map[docFieldKey][]any{},
 						)
