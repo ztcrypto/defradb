ddd63b36204de42b5436ba55bffcb87e40834b97
Islam Aleiv <islam@source.network>
2023-08-18 11:53:51 +0200

    Make combination of index and non-index filters work

--- tests/integration/utils2.go
+++ tests/integration/utils2.go
@@ -1601,7 +1601,6 @@ func executeSubscriptionRequest(
 							action.ExpectedError,
 							nil,
 							// anyof is not yet supported by subscription requests
-							nil,
 							0,
 							map[docFieldKey][]any{},
 						)
@@ -1695,7 +1694,14 @@ func assertRequestResults(
 
 	log.Info(s.ctx, "", logging.NewKV("RequestResults", result.Data))
 
-	require.Equal(s.t, len(expectedResults), len(resultantData), s.testCase.Description)
+	// compare results
+	require.Equal(s.t, len(expectedResults), len(resultantData),
+		s.testCase.Description+" \n(number of results don't match)")
+	if len(expectedResults) == 0 {
+		// Need `require` here otherwise will panic in the for loop that ranges over
+		// resultantData and tries to access expectedResults[0].
+		require.Equal(s.t, expectedResults, resultantData)
+	}
 
 	for docIndex, result := range resultantData {
 		expectedResult := expectedResults[docIndex]
